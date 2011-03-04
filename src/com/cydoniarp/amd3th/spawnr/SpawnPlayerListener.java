6ef3bf63a6e512774548d53aef9a35926ed6527f
Stéphan Kochen <stephan@kochen.nl>
2011-03-05 00:35:32 +0100

    Use getConfiguration, and a single spawn point across worlds.

--- src/com/cydoniarp/amd3th/spawnr/SpawnPlayerListener.java
+++ src/com/cydoniarp/amd3th/spawnr/SpawnPlayerListener.java
@@ -1,6 +1,5 @@
 package com.cydoniarp.amd3th.spawnr;
 
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -15,46 +14,11 @@ public class SpawnPlayerListener extends PlayerListener {
 	
 	public void onPlayerJoin(PlayerEvent event) {
 		Player player = event.getPlayer();
-		Property prop = plugin.getWorldProperty(player.getWorld());
-		if (!prop.keyExists("x")) {
-			prop.setString("x", " ");
-			prop.setString("y", " ");
-			prop.setString("z", " ");
-			prop.setString("yaw", " ");
-			Location oLoc = player.getWorld().getSpawnLocation();
-			player.teleportTo(oLoc);
-			if(player.isOp()) {
-				player.sendMessage("Spawnr point needs to be set.");
-			}
-		}
-		else if (!prop.keyExists(player.getName())) {
-			Location loc = player.getLocation();
-			loc.setX(prop.getDouble("x"));
-			loc.setY(prop.getDouble("y"));
-			loc.setZ(prop.getDouble("z"));
-			loc.setYaw(prop.getFloat("yaw"));
-			player.teleportTo(loc);
-			prop.setBoolean(player.getName(), true);
-		}
+		plugin.teleportToSpawn(player);
 	}
 	
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
 		Player player = event.getPlayer();
-		Property prop = plugin.getWorldProperty(player.getWorld());
-		if (prop.keyExists("x")){
-			Location loc = player.getLocation();
-			loc.setX(prop.getDouble("x"));
-			loc.setY(prop.getDouble("y"));
-			loc.setZ(prop.getDouble("z"));
-			loc.setYaw(prop.getFloat("yaw"));
-			event.setRespawnLocation(loc);
-		}
-		else {
-			Location oLoc = player.getWorld().getSpawnLocation();
-			player.teleportTo(oLoc);
-			if(player.isOp()){
-				player.sendMessage("Spawnr point needs to be set.");
-			}
-		}
+		plugin.teleportToSpawn(player);
 	}
 }
--- src/com/cydoniarp/amd3th/spawnr/Spawnr.java
+++ src/com/cydoniarp/amd3th/spawnr/Spawnr.java
@@ -1,11 +1,8 @@
 package com.cydoniarp.amd3th.spawnr;
 
-import java.io.File;
-import java.util.HashMap;
 import java.util.logging.Logger;
 
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -13,32 +10,23 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 
 public class Spawnr extends JavaPlugin {
 	private final SpawnPlayerListener pListener = new SpawnPlayerListener(this);
 	private static Logger log = Logger.getLogger("Minecraft");
 	
-	private HashMap<Long, Property> properties;
-	
-	public Property getWorldProperty(World world) {
-		long worldId = world.getId();
-		Property result = properties.get(worldId);
-		if (result == null) {
-			result = new Property(new File(getDataFolder(), worldId + ".spawn"));
-			properties.put(worldId, result);
-		}
-		return result;
-	}
-	
 	public void onEnable() {
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_JOIN, this.pListener, Event.Priority.Normal, this);
 		pm.registerEvent(Event.Type.PLAYER_RESPAWN, this.pListener, Event.Priority.Normal, this);
+		
+		if (!getDataFolder().isDirectory())
+			getDataFolder().mkdir();
+		getConfiguration().load();
+		
 		PluginDescriptionFile pdf = this.getDescription();
 		log.info("["+pdf.getName()+"] v"+pdf.getVersion()+" has been enabled");
-		if (!(new File("plugins/Spawnr").isDirectory())) {
-			(new File("plugins/Spawnr")).mkdir();
-		}
 	}
 	
 	public void onDisable() {
@@ -54,36 +42,39 @@ public class Spawnr extends JavaPlugin {
 		}
 		
 		Player player = (Player)sender;
-		Property prop = getWorldProperty(player.getWorld());
 		if (cmdName.equalsIgnoreCase("spawnr")) {
 			if (!player.isOp()) {
 				player.sendMessage("You are not OP");
 				return true;
 			}
+			Configuration conf = getConfiguration();
 			Location loc = player.getLocation();
-			prop.setDouble("x", loc.getX());
-			prop.setDouble("y", loc.getY());
-			prop.setDouble("z", loc.getZ());
-			prop.setFloat("yaw", loc.getYaw());
+			conf.setProperty("world", loc.getWorld().getName());
+			conf.setProperty("x", loc.getX());
+			conf.setProperty("y", loc.getY());
+			conf.setProperty("z", loc.getZ());
+			conf.setProperty("yaw", loc.getYaw());
 			player.sendMessage("Spawnr point set.");
 			return true;
 		}
 
 		if (cmdName.equalsIgnoreCase("spawn")) {
-			if (!prop.keyExists("x")) {
-				player.sendMessage("No point to teleport to.");
-				return true;
-			}
-			Location locS = player.getLocation();
-			locS.setX(prop.getDouble("x"));
-			locS.setY(prop.getDouble("y"));
-			locS.setZ(prop.getDouble("z"));
-			locS.setYaw(prop.getFloat("yaw"));
-			player.teleportTo(locS);
-			player.sendMessage("Teleported!");
+			teleportToSpawn(player);
 			return true;
 		}
 
 		return false;
+	}
+	
+	public void teleportToSpawn(Player player) {
+		Configuration conf = getConfiguration();
+		Location locS = player.getLocation();
+		locS.setWorld(getServer().getWorld(conf.getString("world", "world")));
+		locS.setX(conf.getDouble("x", 0));
+		locS.setY(conf.getDouble("y", 64));
+		locS.setZ(conf.getDouble("z", 0));
+		locS.setYaw((float)conf.getDouble("yaw", 0));
+		player.teleportTo(locS);
+		player.sendMessage("Teleported!");
 	}
 }
